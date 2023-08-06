469ddea19414f29dea8882717783580e53d6815f
yashwanth3 <yashwanthvemuganti@gmail.com>
2023-08-06 17:18:58 -0400

    spring_reditt_clone

---
+++ spring-reddit-clone/src/test/java/com/example/reddit/springredditclone/SpringRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddit.springredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
